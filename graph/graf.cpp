2195fef83124d520bd15de61bef6321c51d2123a
Neko_LPN <laptevpavel2007@gmail.com>
2026-05-04 19:50:05 +0800

    update graph

--- graph/graf.cpp
+++ graph/graf.cpp
@@ -21,7 +21,9 @@ bool Node::isNeighbour(Node* node) const {
 
 int Node::getWeight(Node* neighbour) const {
     auto it = weights.find(neighbour);
-    if (it != weights.end()) return it->second;
+    if (it != weights.end()) {
+        return it->second;
+    }
     return INT_MAX;
 }
 
@@ -34,11 +36,15 @@ Node::neighbour_iterator Node::nb_end() const {
 }
 
 void Graph::addNode(Node* node) {
-    if (node) nodes.insert(node);
+    if (node) {
+        nodes.insert(node);
+    }
 }
 
 void Graph::removeNode(Node* node) {
-    if (!node) return;
+    if (!node) {
+        return;
+    }
     nodes.erase(node);
     for (node_iterator it = nodes.begin(); it != nodes.end(); ++it) {
         (*it)->removeNeighbour(node);
@@ -46,14 +52,17 @@ void Graph::removeNode(Node* node) {
 }
 
 void Graph::addEdge(Node* begin, Node* end, int weight) {
-    if (nodes.find(begin) == nodes.end() || nodes.find(end) == nodes.end())
+    if (nodes.find(begin) == nodes.end() || nodes.find(end) == nodes.end()) {
         return;
+    }
     begin->addNeighbour(end, weight);
     end->addNeighbour(begin, weight);
 }
 
 void Graph::removeEdge(Node* begin, Node* end) {
-    if (!begin || !end) return;
+    if (!begin || !end) {
+        return;
+    }
     begin->removeNeighbour(end);
     end->removeNeighbour(begin);
 }
@@ -69,15 +78,20 @@ Graph::node_iterator Graph::end() const {
 BFS::BFS(const Graph& agraph) : graph(agraph) {}
 
 bool BFS::connected(Node* begin, Node* end) {
-    if (!begin || !end) return false;
+    if (!begin || !end) {
+        return false;
+    }
     std::queue<Node*> q;
     std::set<Node*> visited;
     q.push(begin);
     visited.insert(begin);
 
     while (!q.empty()) {
-        Node* curr = q.front(); q.pop();
-        if (curr == end) return true;
+        Node* curr = q.front();
+        q.pop();
+        if (curr == end) {
+            return true;
+        }
         for (Node::neighbour_iterator it = curr->nb_begin(); it != curr->nb_end(); ++it) {
             Node* nb = it->first;
             if (visited.find(nb) == visited.end()) {
@@ -92,12 +106,16 @@ bool BFS::connected(Node* begin, Node* end) {
 DFS::DFS(const Graph& agraph) : graph(agraph) {}
 
 bool DFS::dfs(Node* begin, Node* end) {
-    if (begin == end) return true;
+    if (begin == end) {
+        return true;
+    }
     visited.insert(begin);
     for (Node::neighbour_iterator it = begin->nb_begin(); it != begin->nb_end(); ++it) {
         Node* nb = it->first;
         if (visited.find(nb) == visited.end()) {
-            if (dfs(nb, end)) return true;
+            if (dfs(nb, end)) {
+                return true;
+            }
         }
     }
     return false;
@@ -115,9 +133,15 @@ MarkedNode::MarkedNode(Node* anode, int amark, Node* aprev)
 void PriorityQueue::push(Node* node, int mark, Node* prev) {
     MarkedNode mn(node, mark, prev);
     std::vector<MarkedNode>::iterator it = nodes.begin();
-    while (it != nodes.end() && mark < it->mark) ++it;
-    if (it == nodes.end()) nodes.push_back(mn);
-    else nodes.insert(it, mn);
+    while (it != nodes.end() && mark < it->mark) {
+        ++it;
+    }
+    if (it == nodes.end()) {
+        nodes.push_back(mn);
+    }
+    else {
+        nodes.insert(it, mn); 
+    }
 }
 
 MarkedNode PriorityQueue::pop() {
@@ -147,7 +171,9 @@ Way unroll(std::map<Node*, MarkedNode>& visited, Node* begin, Node* curr) {
 Dijkstra::Dijkstra(const Graph& agraph) : graph(agraph) {}
 
 Way Dijkstra::shortestWay(Node* begin, Node* end) {
-    if (!begin || !end) return Way();
+    if (!begin || !end) {
+        return Way();
+    }
 
     PriorityQueue nodes;
     nodes.push(begin, 0, 0);
@@ -164,7 +190,9 @@ Way Dijkstra::shortestWay(Node* begin, Node* end) {
         for (Node::neighbour_iterator it = cur.node->nb_begin(); it != cur.node->nb_end(); ++it) {
             Node* nb = it->first;
             int weight = nb->getWeight(cur.node);
-            if (weight == INT_MAX) continue;
+            if (weight == INT_MAX) {
+                continue;
+            }
             int newMark = cur.mark + weight;
 
             if (visited.find(nb) == visited.end()) {
